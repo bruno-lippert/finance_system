773b85d8f72edd3c8ae63f6a56a14ede21a6f16b
Bruno Lippert <brunolippert2002@gmail.com>
2023-09-19 17:47:13 -0300

    feat: included react-toastify in the application

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { userLogin, userSingUp } from '../../services/userManagement';
 import { UserLogin } from '../../types/Login';
 import { BsArrowLeftCircle } from 'react-icons/bs';
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -30,14 +32,20 @@ export default function Login() {
       if (user.email === email && user.password === password) {
         navigate('/home')
       } else {
-        setError("Usuário não encontrado!")
+        setError(`Usuário não encontrado!`)
+        toastError(`Usuário não encontrado!`)
       }
     })
 
   }
 
   const handleUserLogin = async () => {
-    fetchLogin()
+    try{
+      fetchLogin()
+    } catch {
+      toastError(`Erro ao logar!`)
+    }
+    
   }
 
   const handleUserSingUp = async () => {
@@ -48,13 +56,17 @@ export default function Login() {
     }
 
     if (!newUser.email.includes('@')) {
-      alert(`Senha deve conter no minimo 8 caracteres!`)
+      toastError(`Email inválido!`)
     } else if (newUser.email === `` && newUser.name === ``) {
-      alert(`Dados inválidos!`)
+      toastError(`Dados inválido!`)
     } else if (Number(newUser.password.length) < 8) {
-      alert(`Email Inválido!`)
+      toastError(`A senha deve conter no mínimo 8 caracteres!`)
     } else {
       userSingUp(newUser)
+      toast.success(`Usuário cadastrado com sucesso!`, {
+        position: toast.POSITION.TOP_RIGHT,
+        theme: 'colored'
+      })
       setUserHasLogin(true)
     }
   }
@@ -71,48 +83,58 @@ export default function Login() {
     setUserName(event.target.value)
   }
 
+  const toastError = (message: string) => {
+    toast.error(message, {
+      position: toast.POSITION.TOP_RIGHT,
+      theme: 'colored'
+    })
+  }
+
   return (
-    <S.Container>
-      <S.Login>
+    <>
+      <S.Container>
+        <S.Login>
 
-        {userHasLogin ?
-          <h1>Login</h1> :
-          <>
-        <S.BackButton>
-          <BsArrowLeftCircle onClick={() => setUserHasLogin(true)} />
-        </S.BackButton>
-            <h1>Cadastro</h1>
-          </>}
+          {userHasLogin ?
+            <h1>Login</h1> :
+            <>
+              <S.BackButton>
+                <BsArrowLeftCircle onClick={() => setUserHasLogin(true)} />
+              </S.BackButton>
+              <h1>Cadastro</h1>
+            </>}
 
-        <div>
-          <label htmlFor="email"><h3>Email:</h3></label>
-          <S.Email onChange={handleEmail} />
-        </div>
+          <div>
+            <label htmlFor="email"><h3>Email:</h3></label>
+            <S.Email onChange={handleEmail} />
+          </div>
 
-        <div>
-          <label htmlFor="passwor"><h3>Senha:</h3></label>
-          <S.Password onChange={handlePassword} />
-        </div>
+          <div>
+            <label htmlFor="passwor"><h3>Senha:</h3></label>
+            <S.Password onChange={handlePassword} />
+          </div>
 
-        {userHasLogin ?
-          <>
-            <S.LoginButton value='Entrar' type='submit' onClick={() => handleUserLogin()} />
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-          </> :
-          <>
-            <div>
-              <label htmlFor="name"><h3>Nome:</h3></label>
-              <S.Name onChange={handleName} />
-            </div>
-            <S.RegisterButton value='Cadastrar' type='submit' onClick={() => handleUserSingUp()} />
-          </>
-        }
+          {userHasLogin ?
+            <>
+              <S.LoginButton value='Entrar' type='submit' onClick={() => handleUserLogin()} />
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+            </> :
+            <>
+              <div>
+                <label htmlFor="name"><h3>Nome:</h3></label>
+                <S.Name onChange={handleName} />
+              </div>
+              <S.RegisterButton value='Cadastrar' type='submit' onClick={() => handleUserSingUp()} />
+            </>
+          }
 
 
-        {userHasLogin && <S.NoRecord onClick={() => setUserHasLogin(false)}>Não tenho cadastro</S.NoRecord>}
+          {userHasLogin && <S.NoRecord onClick={() => setUserHasLogin(false)}>Não tenho cadastro</S.NoRecord>}
 
-      </S.Login>
-    </S.Container>
+        </S.Login>
+      </S.Container>
+      <ToastContainer />
+    </>
 
   );
 }
