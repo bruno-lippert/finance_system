8f7cec1fc18dcfad1efee0cbb5dc9590cd4db971
Bruno Lippert <brunolippert2002@gmail.com>
2023-09-27 09:49:00 -0300

    feat: add user reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+    </>
+  )
 }
 
 export default App;
