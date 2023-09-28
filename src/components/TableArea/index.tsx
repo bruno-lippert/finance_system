0e44207da14d6fdaa7e43c232582148e3a4ad23e
Bruno Lippert <brunolippert2002@gmail.com>
2023-09-28 08:43:35 -0300

    feat: added pagination

--- src/components/TableArea/index.tsx
+++ src/components/TableArea/index.tsx
@@ -1,6 +1,8 @@
 import * as S from './styles'
 import { Item } from '../../types/Item'
 import TableItem from '../TableItem'
+import { useState } from 'react'
+import { MdArrowForwardIos, MdArrowBackIos } from 'react-icons/md'
 
 type Props = {
   itemList: Item[],
@@ -10,28 +12,61 @@ type Props = {
 }
 
 export default function TableArea({ itemList, handleRemoveBond, setEditModal, setItem }: Props) {
-  
+  const itemsPerPage = 10; // Número de itens por página
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  // Filtrar os itens com base na página atual
+  const itemsToDisplay = itemList.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(itemList.length / itemsPerPage);
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handlePreviousPage = () => {
+    if(currentPage !== 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
+  const handleNextPage = () => {
+    if(currentPage !== totalPages){
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
   return (
-    <S.Table>
+    <S.TableContainer>
+      <S.Table>
         <thead>
-            <tr>
-                <S.TableHeadColumn width={20}>Data</S.TableHeadColumn>
-                <S.TableHeadColumn width={20}>Categoria</S.TableHeadColumn>
-                <S.TableHeadColumn width={30}>Título</S.TableHeadColumn>
-                <S.TableHeadColumn width={20}>Valor</S.TableHeadColumn>
-                <S.TableHeadColumn width={10}/>
-            </tr>
+          <tr>
+            <S.TableHeadColumn width={20}>Data</S.TableHeadColumn>
+            <S.TableHeadColumn width={20}>Categoria</S.TableHeadColumn>
+            <S.TableHeadColumn width={30}>Título</S.TableHeadColumn>
+            <S.TableHeadColumn width={20}>Valor</S.TableHeadColumn>
+            <S.TableHeadColumn width={10} />
+          </tr>
         </thead>
         <tbody>
-          {itemList.map((item, index) => (
-              <TableItem
+          {itemsToDisplay.map((item, index) => (
+            <TableItem
               key={index}
-              position={index}
               item={item}
               handleRemoveBond={handleRemoveBond}
-              setEditModal={setEditModal} setItem={setItem}/>
+              setEditModal={setEditModal} setItem={setItem} />
           ))}
         </tbody>
-    </S.Table>
+      </S.Table>
+
+      <S.PageControlContainer>
+        <S.PreviousPage onClick={handlePreviousPage}><MdArrowBackIos /></S.PreviousPage>
+        <S.PageControl>{currentPage} de {totalPages}</S.PageControl>
+        <S.NextPage onClick={handleNextPage}><MdArrowForwardIos /></S.NextPage>
+      </S.PageControlContainer>
+    </S.TableContainer>
   )
 }
--- src/components/TableArea/styles.tsx
+++ src/components/TableArea/styles.tsx
@@ -1,10 +1,16 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const TableContainer = styled.main`
+    width: 100%;
+    box-shadow: 0px 0px 5px #CCC;
+    border-radius: 10px;
+`;
 
 export const Table = styled.table`
-    box-shadow: 0px 0px 5px #CCC;
     padding: 5px;
-    border-radius: 10px;
     text-align: left;
+    width: 100%;
+
     @media (max-width: 450px) {
         font-size: 0.8rem;
     }
@@ -16,4 +22,39 @@ export const Table = styled.table`
 export const TableHeadColumn = styled.th<{ width?: number }>`
     width: ${ props => props.width ? `${props.width}%` : `auto`} ;
     padding: 10px;
+`;
+
+export const PageControlContainer = styled.section`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 5px;
+    padding: 5px 10px 10px 5px;
+
+    * {
+        margin: 0;
+        padding: 0;
+    }
+`;
+
+const GlobalPageControlContainer = css`
+    width: 45px;
+    height: 25px;
+    text-align: center;
+`;
+
+export const NextPage = styled.button`
+    width: 35px;
+    height: 25px;
+`;
+
+export const PreviousPage = styled.button`
+    width: 35px;
+    height: 25px;
+`;
+
+export const PageControl = styled.div`
+    width: 45px;
+    height: 25px;
+    text-align: center;
 `;
